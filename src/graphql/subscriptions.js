e88885047ee1eb2f89f60adfe0acf2f0876ced37
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2019-08-14 11:24:24 +0000

    changes up to step 4 of tutoral (post deploy of API)

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateComment = `subscription OnCreateComment {
+  onCreateComment {
+    id
+    postPath
+    timestamp
+  }
+}
+`;
+export const onUpdateComment = `subscription OnUpdateComment {
+  onUpdateComment {
+    id
+    postPath
+    timestamp
+  }
+}
+`;
+export const onDeleteComment = `subscription OnDeleteComment {
+  onDeleteComment {
+    id
+    postPath
+    timestamp
+  }
+}
+`;
